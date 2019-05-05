deab313bad2e6e55eaecbb0dcdb38af9734f7538
Nate Donato <natedonato@gmail.com>
2019-05-05 12:04:47 -0700

    attempting a viewport to fix mobile layout better

--- frontend/components/photo/photo_form.jsx
+++ frontend/components/photo/photo_form.jsx
@@ -5,15 +5,29 @@ import { withRouter } from 'react-router-dom';
 class PhotoForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = this.props.currentUser;
+        this.state = {
+            photoFile: null,
+            comment: ""
+        };
     }
 
     handleFile(e) {
-        this.setState({ photoFile: e.currentTarget.files[0] });
+        this.setState({ 
+            photoFile: e.currentTarget.files[0],
+            comment: ""
+        });
+    }
+
+    update(field) {
+        return (
+            (e) => this.setState({ [field]: e.target.value })
+        );
     }
 
     handleSubmit(e) {
         e.preventDefault();
+        if(this.state.photoFile === null){
+            alert("Upload Failed - No Photo File Detected! \n \t Please make sure you have attached a photo and try again. \t");}
         const formData = new FormData();
         formData.append('photo[pic]', this.state.photoFile);
         this.props.postPhoto(formData).then((result) => this.props.history.push(`/photos/${result.data.photo.id}`
@@ -25,6 +39,11 @@ class PhotoForm extends React.Component {
             <div>
                 <form action="">
                     <input type="file" onChange={this.handleFile.bind(this)} />
+                    <input className="inputfields"
+                        type="text"
+                        value={`${this.state.comment}`}
+                        onChange={this.update("comment")}
+                        placeholder="Add Caption (optional)" />
                     <input type="submit" value="submit" onClick={this.handleSubmit.bind(this)} />
                 </form>
             </div>
