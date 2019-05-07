eb06328c8bd089881b33676dbd9618a9cc6aa0d1
Nate Donato <natedonato@gmail.com>
2019-05-07 16:20:55 -0700

    starts user profile style

--- frontend/components/current_user/user_profile.jsx
+++ frontend/components/current_user/user_profile.jsx
@@ -29,14 +29,13 @@ class UserProfile extends React.Component {
                 return(
                 <div>
                     <EditProfilePicContainer currentUser={this.props.currentUser} />
-                    <button className="header-button" onClick={this.props.logout}>Log Out</button>
+                    {/* <button className="header-button" onClick={this.props.logout}>Log Out</button> */}
                 </div>)
             } else {
                 let user = this.props.users[this.props.id]
                 return(
                     <div>
                         <img className="profilePic" src={`${user.picUrl}`} alt="" />
-                        {user.username}
                     </div>
                 )
             }
@@ -46,8 +45,25 @@ class UserProfile extends React.Component {
         
  
         return(
-            <div className="wholeProfileContainer">
+            <div className="wholeProfileContainer">   
+                <div className="profilePicContainer">
                 {renderUserInfo()}
+                </div>            
+                <div className="ProfileInfo">
+                <div className="profileHead">
+                {this.props.users[this.props.id].username}
+                </div>
+                <div className="profileStats">
+                    485 posts 351 followers 560 following
+                </div>
+                <div className="profileBio">
+                Full Name
+                <br></br>
+                Bio
+                {this.props.users[this.props.id].full_name}
+                {this.props.users[this.props.id].bio}
+                </div> 
+                </div>
             </div>
         );
     }
